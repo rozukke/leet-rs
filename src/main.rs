d76221805a3c793d6505129ab2bee2379feb2fa1
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-09-03 19:48:46 +1000

    Init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// Put any use statements here to allow copy-pasting up to the end of the impl Solution block
+
+impl Solution {
+    // Workspace here
+}
+
+fn main() {
+    // Workspace here
+}
+
+struct Solution;
