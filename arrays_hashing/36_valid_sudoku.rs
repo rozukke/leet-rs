4fe2827a4c0e2f497898033aba9b05dd55e629aa
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2024-09-05 12:14:24 +1000

    Valid sudoku

---
+++ arrays_hashing/36_valid_sudoku.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+
+impl Solution {
+    // TODO: This can be improved using bit manipulation, will do later
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        // Have data structures for the three rules
+        // Validate rows and columns using idx
+        // Validate squares by using a tuple pair
+        let mut rows = [[false; 10]; 10];
+        let mut cols = [[false; 10]; 10];
+        let mut sqrs: HashMap<(usize, usize), [bool; 10]> = HashMap::new();
+
+        for (i, row) in board.into_iter().enumerate() {
+            for (j, item) in row.into_iter().enumerate() {
+                match item {
+                    '.' => continue,
+                    c => {
+                        // Convert to digit
+                        let c = (c as u8 - b'0') as usize;
+                        // Get square entry
+                        let ent = sqrs.entry((i / 3, j / 3)).or_insert([false; 10]);
+                        // Check if exists
+                        if rows[i][c] || cols[j][c] || ent[c] {
+                            return false;
+                        }
+                        // Set digits
+                        rows[i][c] = true;
+                        cols[j][c] = true;
+                        ent[c] = true;
+                    }
+                }
+            }
+        }
+        true
+    }
+}
